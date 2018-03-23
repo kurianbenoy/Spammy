70784f7fb50c3f66d0d92a24e33ab437e864ffe7
kurianbenoy <kurian.pro@gmail.com>
2018-03-23 23:27:57 +0530

    First program

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.genric import TemplateView
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "core/spamsubmission.html"
