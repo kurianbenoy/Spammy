1dc5843360211fcfee72ab86f7e4fea3aa0bf880
kurianbenoy <kurian.pro@gmail.com>
2018-03-28 06:03:34 +0530

    Add requirements.txt

--- core/forms.py
+++ core/forms.py
@@ -1,2 +1,9 @@
-from django import forms
+from django.forms import ModelForm
 from .models import Classifer
+
+
+class ClassiferForm(ModelForm):
+    class Meta:
+        model = Classifer
+        fields = [input]
+        
