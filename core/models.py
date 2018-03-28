d8fe37fbd778e6d5c78661779cd3ce33b0997341
kurianbenoy <kurian.pro@gmail.com>
2018-03-28 05:59:30 +0530

    Add models and forms

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Classifer(models.Model):
+    input = models.TextField()
+    times = models.DateField()
+
+    def date_status(self):
+        if self.times < datetime.date(2018,4,4):
+            return "Testing model"
+        if self.times > datetime.date(2018,4,4):
+            return "Training data"
+
+    def __str__(self):
+        return self.name
