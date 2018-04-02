931095889b1068bb2bb295f7516261a220468e44
kurianbenoy <kurian.pro@gmail.com>
2018-04-02 07:11:44 -0500

    Modified main spam program

--- python/spam.py
+++ python/spam.py
@@ -14,13 +14,18 @@ y = dataset.iloc[:, 1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit(X_train)
-X_test = sc.transform(X_test)
-print(X_train)
-print(X_test)
+
+# Fitting classifier to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train,y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
 # CountVectorizer method
 # from sklearn.feature_extraction.text import CountVectorizer
