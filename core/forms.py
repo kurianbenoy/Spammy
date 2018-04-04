3cfe79a9b247d5985e822602aa1e04239fd2861e
kurianbenoy <kurian.pro@gmail.com>
2018-04-04 08:13:05 -0500

    Made spam submission form
    It was made by django default widget forms

--- core/forms.py
+++ core/forms.py
@@ -3,8 +3,13 @@ from django.forms import ModelForm
 from .models import Classifer
 
 
-class ClassifierForm(ModelForm):
+class ClassifierForm(ModelForm):	
+
     class Meta:
         model = Classifer
         fields = ['inputtext',]
-        
+    
+
+    def __init__(self,*args,**kwargs):
+    	super().__init__(*args,**kwargs)
+    	self.fields['inputtext'].widget.attrs={'class':'spamform'}    
